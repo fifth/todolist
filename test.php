d998716aa203927ae2232f38a03fe6fc97a1170e
Yu Jiechao <fifth0725@gmail.com>
2014-10-18 01:07:38 +0800

    using regular expression

--- test.php
+++ test.php
@@ -2,26 +2,52 @@
 	$todolist=array();
 	$re=array();
 	$content=file_get_contents("https://github.com/fifth/todolist/wiki");
-	$content=strstr($content, '<ul class="wiki-pages"');
-	$content=substr($content, 0, strpos($content, '</ul>')+4);
-	$content=strip_tags($content,"<a>");
-	while (strpos($content, "<a")) {
-		$tmp=substr($content, 0, strpos($content, '</a>')+4);
-		$title=strip_tags($tmp);
-		$url="https://github.com".substr($tmp, strpos($tmp, '"')+1, strrpos($tmp, '"')-strpos($tmp, '"')-1);
-		if (($url!='https://github.com/fifth/todolist/wiki')&&($url!='https://github.com/fifth/todolist/wiki/wiki%E6%A8%A1%E6%9D%BF%E9%A1%B5%E9%9D%A2')) {
-			$todolist[]=array("title"=>$title, "url"=>$url);
+	if (preg_match("/\<ul\sclass=\"wiki-pages\"[\s\S]*?\<\/ul\>/", $content, $matches)) {
+		$content=$matches[0];
+	}
+	if (preg_match_all("/\<a[\s\S]*?\<\/a\>/", $content, $matches)) {
+		$content=$matches[0];
+		array_pop($content);
+		foreach ($content as $key => $value) {
+			if ((preg_match("/(?<=\")[\s\S]*(?=\")/", $value, $url))&&($url[0]!="/fifth/todolist/wiki")&&($url[0]!="/fifth/todolist/wiki/wiki%E6%A8%A1%E6%9D%BF%E9%A1%B5%E9%9D%A2")&&(preg_match("/(?<=\>)[\s\S]*(?=\<\/a\>)/", $value, $title))) {
+				$todolist[]=array(
+					"url" => "https://github.com".$url[0],
+					"title" => $title[0]
+				);
+			}
 		}
-		$content=substr($content, strpos($content, '</a>')+4);
 	}
-	array_pop($todolist);
 	foreach ($todolist as $key => $value) {
+		// $re[$key]=array();
 		$content=file_get_contents($value['url']);
-		echo $content;
-		die();
-		// $re[$key]=
+		if (preg_match("/\<div\sclass=\"markdown-body\"\>[\s\S]*?\<\/div\>/", $content, $matches)) {
+			$content=$matches[0];
+		}
+		if (preg_match("/(?<=\<\/a\>)[\s\S]*?(?=\<\/h1\>)/", $content, $matches)) {
+			$re[$key]['title-zh']=$matches[0];
+			preg_match("/(?<=\<\/h1\>)[\s\S]*/", $content, $matches);
+			$content=$matches[0];
+		}
+		if (preg_match("/(?<=\<\/a\>)[\s\S]*?(?=\<\/h2\>)/", $content, $matches)) {
+			$re[$key]['title-jp']=$matches[0];
+			preg_match("/(?<=\<\/h2\>)[\s\S]*/", $content, $matches);
+			$content=$matches[0];
+		}
+		if (preg_match("/(?<=\<\/a\>)[\s\S]*?(?=\<\/h3\>)/", $content, $matches)) {
+			$re[$key]['title-en']=$matches[0];
+			preg_match("/(?<=\<\/h3\>)[\s\S]*/", $content, $matches);
+			$content=$matches[0];
+		}
+		if (preg_match_all("/\<p\>[\s\S]*?\<\/p\>/", $content, $matches)) {
+			print_r(count($matches[0]));
+			print_r("<br/>");
+		}
+		if ($key==3) {
+			print_r($re[$key]['title-zh']);
+			print_r($re[$key]['title-jp']);
+			print_r($re[$key]['title-en']);
+			print_r($content);
+			die();	
+		}
 	}
-	echo json_encode($todolist);
-	// print_r(substr_count($content, "<a"));
-	// print_r($todolist);
 ?>
